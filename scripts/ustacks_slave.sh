131ed5f07daf8fb75ca14155ef1b67c656755c88
alk224 <alk224@nau.edu>
2016-04-19 13:33:03 -0700

    ustacks slave

---
+++ scripts/ustacks_slave.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+#
+#  ustacks_slave.sh - ustacks script for RADseq workflow
+#
+#  Version 0.9 (April 19, 2016)
+#
+#  Copyright (c) 2015-2016 Andrew Krohn
+#
+#  This software is provided 'as-is', without any express or implied
+#  warranty. In no event will the authors be held liable for any damages
+#  arising from the use of this software.
+#
+#  Permission is granted to anyone to use this software for any purpose,
+#  including commercial applications, and to alter it and redistribute it
+#  freely, subject to the following restrictions:
+#
+#  1. The origin of this software must not be misrepresented; you must not
+#     claim that you wrote the original software. If you use this software
+#     in a product, an acknowledgment in the product documentation would be
+#     appreciated but is not required.
+#  2. Altered source versions must be plainly marked as such, and must not be
+#     misrepresented as being the original software.
+#  3. This notice may not be removed or altered from any source distribution.
+#
+## Trap function on exit.
+function finish {
+if [[ -f $stdout ]]; then
+	rm $stdout
+fi
+if [[ -f $stderr ]]; then
+	rm $stderr
+fi
+}
+trap finish EXIT
+
+## Define input variables
+	scriptdir="$(cd "$(dirname "$0")" && pwd)"
+	repodir=`dirname $scriptdir`
+	tempdir="$repodir/temp/"
+	workdir=$(pwd)
+
+	stdout=($1)
+	stderr=($2)
+	randcode=($3)
+	config=($4)
+	outdir=($5)
+	outdirunc=($6)
+	repfile=($7)
+
+## Read additional variables from config file
+	cores=(`grep "CPU_cores" $config | grep -v "#" | cut -f 2`)
+
+## Ustacks command
+	mkdir -p $outdirunc/dereplicated_ustacks_output
+
+		for line in `cat $repfile | cut -f1`; do
+		sqlid=$(cat /dev/urandom |tr -dc '0-9' | fold -w 8 | head -n 1)
+		echo "  ustacks -t fastq -f $workdir/demult-derep_output/dereplicated_combined_data/${line}.fq -p $cores -o $outdirunc/dereplicated_ustacks_output -i $sqlid -m 2 -M 4 -N 6 -r -d" >> $log
+		ustacks -t fastq -f $workdir/demult-derep_output/dereplicated_combined_data/${line}.fq -p $cores -o $outdirunc/dereplicated_ustacks_output -i $sqlid -m 2 -M 4 -N 6 -r -d &> $outdirunc/dereplicated_ustacks_output/log_${line}_ustacks.txt
+		done
+
+exit 0
