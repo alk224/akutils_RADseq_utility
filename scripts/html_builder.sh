d55937c306ccfbe990ca73b48617ac5ca7609e15
alk224 <alk224@nau.edu>
2016-04-23 19:15:52 -0700

    additions to alternative output

--- scripts/html_builder.sh
+++ scripts/html_builder.sh
@@ -53,81 +53,197 @@ trap finish EXIT
 	#output name
 	sed -i "s/<!--anchor001-->/$db/g" $html
 
+	#sumstats
+	ssfilename="batch_${batch}.sumstats.tsv"
+	ssfile="$outdir/$ssfilename"
+	ssfile2="$outdir2/$ssfilename"
+	cp $ssfile $outdir3/html/
+	ssfile3="$outdir3/html/$ssfilename"
+	if [[ -f "$ssfile" ]]; then 2>/dev/null
+	sed -i "s|<!--anchor002-->|$ssfilename|" $html
+	sed -i "s/<!--anchor002a-->/$ssfilename/" $html
+	fi
+
+	#sumsumstats
+	sssfilename="batch_${batch}.sumstats_summary.tsv"
+	sssfile="$outdir/$sssfilename"
+	sssfile2="$outdir2/$sssfilename"
+	cp $sssfile $outdir3/html/ 2>/dev/null
+	sssfile3="$outdir3/html/$sssfilename"
+	if [[ -f "$sssfile" ]]; then
+	sed -i "s|<!--anchor003-->|$sssfilename|" $html
+	sed -i "s/<!--anchor003a-->/$sssfilename/" $html
+	fi
+
+	#fst
+	fstfilename="batch_${batch}.fst_summary.tsv"
+	fstfile="$outdir/$fstfilename"
+	fstfile2="$outdir2/$fstfilename"
+	cp $fstfile $outdir3/html/ 2>/dev/null
+	fstfile3="$outdir3/html/$fstfilename"
+	if [[ -f "$fstfile" ]]; then
+	sed -i "s|<!--anchor004-->|$fstfilename|" $html
+	sed -i "s/<!--anchor004a-->/$fstfilename/" $html
+	fi
+
+	#fstpop
+	cd $outdir
+	fstpopfilename=$(ls batch_${batch}.fst_* 2>/dev/null | grep -v batch_${batch}.fst_summary.tsv 2>/dev/null)
+	wait
+	cd $workdir
+	fstpopfile="$outdir/$fstpopfilename"
+	fstpopfile2="$outdir2/$fstpopfilename"
+	cp $fstpopfile $outdir3/html/ 2>/dev/null
+	fstpopfile3="$outdir3/html/$fstpopfilename"
+	if [[ -f "$fstpopfile" ]]; then
+	sed -i "s|<!--anchor005-->|$fstpopfilename|" $html
+	sed -i "s/<!--anchor005a-->/$fstpopfilename/" $html
+	fi
+
+	#phi
+	phifilename="batch_${batch}.phistats.tsv"
+	phifile="$outdir/$phifilename"
+	phifile2="$outdir2/$phifilename"
+	cp $phifile $outdir3/html/ 2>/dev/null
+	phifile3="$outdir3/html/$phifilename"
+	if [[ -f "$phifile" ]]; then
+	sed -i "s|<!--anchor006-->|$phifilename|" $html
+	sed -i "s/<!--anchor006a-->/$phifilename/" $html
+	fi
+
+	#phipop
+	cd $outdir
+	phipopfilename=$(ls batch_${batch}.phistats_*.tsv 2>/dev/null)
+	wait
+	cd $workdir
+	phipopfile="$outdir/$phipopfilename"
+	phipopfile2="$outdir2/$phipopfilename"
+	cp $phipopfile $outdir3/html/ 2>/dev/null
+	phipopfile3="$outdir3/html/$phipopfilename"
+	if [[ -f "$phipopfile" ]]; then
+	sed -i "s|<!--anchor007-->|$phipopfilename|" $html
+	sed -i "s/<!--anchor007a-->/$phipopfilename/" $html
+	fi
+
 	#structure
 	structurefilename="batch_${batch}.structure.tsv"
 	structurefile="$outdir/$structurefilename"
 	structurefile2="$outdir2/$structurefilename"
-	cp $structurefile $outdir3/html/
+	cp $structurefile $outdir3/html/ 2>/dev/null
 	structurefile3="$outdir3/html/$structurefilename"
 	if [[ -f "$structurefile" ]]; then
-	sed -i "s|<!--anchor002-->|$structurefilename|" $html
-	sed -i "s/<!--anchor002a-->/$structurefilename/" $html
+	sed -i "s|<!--anchor008-->|$structurefilename|" $html
+	sed -i "s/<!--anchor008a-->/$structurefilename/" $html
 	fi
 
 	#plink map
 	plinkmapfilename="batch_${batch}.plink.map"
 	plinkmapfile="$outdir/$plinkmapfilename"
 	plinkmapfile2="$outdir2/$plinkmapfilename"
-	cp $plinkmapfile $outdir3/html/
+	cp $plinkmapfile $outdir3/html/ 2>/dev/null
 	plinkmapfile3="$outdir3/html/$plinkmapfilename"
 	if [[ -f "$plinkmapfile" ]]; then
-	sed -i "s|<!--anchor003-->|$plinkmapfilename|" $html
-	sed -i "s/<!--anchor003a-->/$plinkmapfilename/" $html
+	sed -i "s|<!--anchor009-->|$plinkmapfilename|" $html
+	sed -i "s/<!--anchor009a-->/$plinkmapfilename/" $html
 	fi
 
 	#plink ped
 	plinkpedfilename="batch_${batch}.plink.ped"
 	plinkpedfile="$outdir/$plinkpedfilename"
 	plinkpedfile2="$outdir2/$plinkpedfilename"
-	cp $plinkpedfile $outdir3/html/
+	cp $plinkpedfile $outdir3/html/ 2>/dev/null
 	plinkpedfile3="$outdir3/html/$plinkpedfilename"
 	if [[ -f "$plinkpedfile" ]]; then
-	sed -i "s|<!--anchor004-->|$plinkpedfilename|" $html
-	sed -i "s/<!--anchor004a-->/$plinkpedfilename/" $html
+	sed -i "s|<!--anchor010-->|$plinkpedfilename|" $html
+	sed -i "s/<!--anchor010a-->/$plinkpedfilename/" $html
 	fi
 
 	#vcf
 	vcffilename="batch_${batch}.vcf"
 	vcffile="$outdir/$vcffilename"
 	vcffile2="$outdir2/$vcffilename"
-	cp $vcffile $outdir3/html/
+	cp $vcffile $outdir3/html/ 2>/dev/null
 	vcffile3="$outdir3/html/$vcffilename"
 	if [[ -f "$vcffile" ]]; then
-	sed -i "s|<!--anchor005-->|$vcffilename|" $html
-	sed -i "s/<!--anchor005a-->/$vcffilename/" $html
+	sed -i "s|<!--anchor011-->|$vcffilename|" $html
+	sed -i "s/<!--anchor011a-->/$vcffilename/" $html
+	fi
+
+	#vcfhap
+	vcfhapfilename="batch_${batch}.haplotypes.vcf"
+	vcfhapfile="$outdir/$vcfhapfilename"
+	vcfhapfile2="$outdir2/$vcfhapfilename"
+	cp $vcfhapfile $outdir3/html/ 2>/dev/null
+	vcfhapfile3="$outdir3/html/$vcfhapfilename"
+	if [[ -f "$vcfhapfile" ]]; then
+	sed -i "s|<!--anchor012-->|$vcfhapfilename|" $html
+	sed -i "s/<!--anchor012a-->/$vcfhapfilename/" $html
 	fi
 
 	#genepop
 	genepopfilename="batch_${batch}.genepop"
 	genepopfile="$outdir/$genepopfilename"
 	genepopfile2="$outdir2/$genepopfilename"
-	cp $genepopfile $outdir3/html/
+	cp $genepopfile $outdir3/html/ 2>/dev/null
 	genepopfile3="$outdir3/html/$genepopfilename"
-	if [[ -f "$vcffile" ]]; then
-	sed -i "s|<!--anchor006-->|$genepopfilename|" $html
-	sed -i "s/<!--anchor006a-->/$genepopfilename/" $html
+	if [[ -f "$genepopfile" ]]; then
+	sed -i "s|<!--anchor013-->|$genepopfilename|" $html
+	sed -i "s/<!--anchor013a-->/$genepopfilename/" $html
 	fi
 
 	#phylip
 	phylipfilename="batch_${batch}.phylip"
 	phylipfile="$outdir/$phylipfilename"
 	phylipfile2="$outdir2/$phylipfilename"
-	cp $phylipfile $outdir3/html/
+	cp $phylipfile $outdir3/html/ 2>/dev/null
 	phylipfile3="$outdir3/html/$phylipfilename"
-	if [[ -f "$vcffile" ]]; then
-	sed -i "s|<!--anchor007-->|$phylipfilename|" $html
-	sed -i "s/<!--anchor007a-->/$phylipfilename/" $html
+	if [[ -f "$phylipfile" ]]; then
+	sed -i "s|<!--anchor014-->|$phylipfilename|" $html
+	sed -i "s/<!--anchor014a-->/$phylipfilename/" $html
+	fi
+
+	#haplotypes
+	hapfilename="batch_${batch}.haplotypes.tsv"
+	hapfile="$outdir/$hapfilename"
+	hapfile2="$outdir2/$hapfilename"
+	cp $hapfile $outdir3/html/ 2>/dev/null
+	hapfile3="$outdir3/html/$hapfilename"
+	if [[ -f "$hapfile" ]]; then
+	sed -i "s|<!--anchor015-->|$hapfilename|" $html
+	sed -i "s/<!--anchor015a-->/$hapfilename/" $html
+	fi
+
+	#haplotypestats
+	hapsfilename="batch_${batch}.hapstats.tsv"
+	hapsfile="$outdir/$hapsfilename"
+	hapsfile2="$outdir2/$hapsfilename"
+	cp $hapsfile $outdir3/html/ 2>/dev/null
+	hapsfile3="$outdir3/html/$hapsfilename"
+	if [[ -f "$hapsfile" ]]; then
+	sed -i "s|<!--anchor016-->|$hapsfilename|" $html
+	sed -i "s/<!--anchor016a-->/$hapsfilename/" $html
+	fi
+
+	#markers
+	markfilename="batch_${batch}.markers.tsv"
+	markfile="$outdir/$markfilename"
+	markfile2="$outdir2/$markfilename"
+	cp $markfile $outdir3/html/ 2>/dev/null
+	markfile3="$outdir3/html/$markfilename"
+	if [[ -f "$markfile" ]]; then
+	sed -i "s|<!--anchor017-->|$markfilename|" $html
+	sed -i "s/<!--anchor017a-->/$markfilename/" $html
 	fi
 
 	#fasta
 	fastafilename="batch_${batch}.fa"
 	fastafile="$outdir/$fastafilename"
 	fastafile2="$outdir2/$fastafilename"
-	cp $fastafile $outdir3/html/
+	cp $fastafile $outdir3/html/ 2>/dev/null
 	fastafile3="$outdir3/html/$fastafilename"
-	if [[ -f "$vcffile" ]]; then
-	sed -i "s|<!--anchor008-->|$fastafilename|" $html
-	sed -i "s/<!--anchor008a-->/$fastafilename/" $html
+	if [[ -f "$fastafile" ]]; then
+	sed -i "s|<!--anchor018-->|$fastafilename|" $html
+	sed -i "s/<!--anchor018a-->/$fastafilename/" $html
 	fi
 
 exit 0
